1382dc31292e43f1770817fe2860c1ffc11dc3aa
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2019-07-26 22:06:57 +0200

    Stop autoinstalling Python deps
    It seems there is a race condition if multiple actions are running in
    parallel, the first one to install dependencies will succeed but others
    fail. Let's stop autoinstalling dependencies, that's sad but eh.
    
    The new pattern is inspired by https://github.com/abatilo/actions-poetry

--- python-tools/entrypoint.sh
+++ python-tools/entrypoint.sh
@@ -2,18 +2,4 @@
 
 set -eu
 
-if [ -f "pyproject.toml" ]; then
-  echo "Installing dependencies with Poetry..."
-  poetry config settings.virtualenvs.create false
-  poetry install
-elif [ -f "Pipfile" ]; then
-  echo "Installing dependencies with Pipenv..."
-  pipenv install --system
-elif [ -f "requirements.txt" ]; then
-  echo "Installing dependencies with pip..."
-  pip install -r requirements.txt
-else
-  echo "No dependencies detected"
-fi
-
 sh -c "$*"
